b3a522027a84bdffdeeca0ddbab05780b4131b2f
Chris Wood <wchristopherwood912@gmail.com>
2023-06-14 17:29:06 -0400

    users can sign in

---
+++ sockets/chat.js
@@ -0,0 +1,7 @@
+module.exports = (io, socket) => {
+  socket.on('new user', (username) => {
+    console.log(`✋ ${username} has joined the chat! ✋`);
+    // Send the username to all clients currently connected
+    io.emit('new user', username);
+  });
+};
