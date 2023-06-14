b381d58214265eacefa45b47e866f4d26af5b6e9
Chris Wood <wchristopherwood912@gmail.com>
2023-06-14 17:34:57 -0400

    users can send messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -3,5 +3,12 @@ module.exports = (io, socket) => {
     console.log(`✋ ${username} has joined the chat! ✋`);
     // Send the username to all clients currently connected
     io.emit('new user', username);
+
+    // Listen for new messages
+    socket.on('new message', (data) => {
+    // Send that data back to ALL clients
+      console.log(`🎤 ${data.sender}: ${data.message} 🎤`);
+      io.emit('new message', data);
+    });
   });
 };
